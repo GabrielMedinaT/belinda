eb6d6dd16722305bc2b51a96411b9cf00d7f6969
xavrodem-code <77444931+xavrodem-code@users.noreply.github.com>
2023-03-26 13:58:03 +0100

    nav y paginas agregadas (sin funciones)

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li>Inicio</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Terapias.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Terapias = () => {
+  return <div>Terapias</div>;
+};
+
+export default Terapias;
